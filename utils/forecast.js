f3a65319c573ccbdee03d47bc0740984b81eac5f
Michael Salvatore <msalvatore22@gmail.com>
2019-05-18 19:56:37 -0400

    implemented object destructing

--- utils/forecast.js
+++ utils/forecast.js
@@ -3,16 +3,16 @@ const request = require('request');
 const forecast = (latitude, longitude, callback) => {
 const url = `https://api.darksky.net/forecast/bc65070ab2dca9c102829a07d8c0c748/${latitude},${longitude}`
 
-request({ url: url, json: true }, (error, response) => {
+request({ url, json: true }, (error, { body }) => {
   if(error){
     callback("Unable to connect to weather service")
-  } else if(response.body.error) {
+  } else if(body.error) {
     callback('Unable to find location.')
   } else {
     callback(undefined, {
-      placename: response.body.daily.data[0].summary,
-      temperature: response.body.currently.temperature,
-      Rainchance:  response.body.currently.precipProbability
+      placename: body.daily.data[0].summary,
+      temperature: body.currently.temperature,
+      Rainchance:  body.currently.precipProbability
     })
   }
 })
